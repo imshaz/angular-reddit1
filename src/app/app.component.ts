194c1b11fa8d146f72d0ee68d524f7aea48d5802
imshaz <imshazbangash@gmail.com>
2021-07-23 23:34:24 +0500

    added form and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-reddit';
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean{
+    console.log("addArticle", title.value, link.value);
+    return false
+  }
 }
