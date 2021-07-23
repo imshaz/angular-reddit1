8ce746b2e8b6ced245a8fa9642ce7627f9cb1a63
imshaz <imshazbangash@gmail.com>
2021-07-23 21:27:25 +0500

    added user-name and user list component

---
+++ src/app/user-name/user-name.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-name',
+  templateUrl: './user-name.component.html',
+  styleUrls: ['./user-name.component.css']
+})
+export class UserNameComponent implements OnInit {
+ @Input() name:string;
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
