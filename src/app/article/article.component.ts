0105520d131a1acb2770ddd2d0196328e063160b
imshaz <imshazbangash@gmail.com>
2021-07-24 15:16:43 +0500

    added article model & minor changes in article component

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  article: Article;
+  constructor() {
+    this.article = new Article('Angular.js', 'https://angular.io', 10);
   }
 
+  upVote() {
+    this.article.voteUp();
+    return false;
+  }
+  downVote() {
+    this.article.voteDown();
+    return false;
+  }
+
+  getDomain() {
+    return this.article.domain();
+  }
+  ngOnInit(): void {}
 }
---
+++ src/app/article/article.model.ts
@@ -0,0 +1,28 @@
+export class Article {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number) {
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp(): void {
+    this.votes = ++this.votes;
+  }
+  voteDown(): void {
+    this.votes = --this.votes;
+  }
+
+  domain(): string | null {
+    try {
+      const domainAndPath = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (error) {
+      console.log('something went wrong');
+      return null;
+    }
+  }
+}
