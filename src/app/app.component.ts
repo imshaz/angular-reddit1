66542be434c7cf465080c2432c67fc28e80fb6aa
imshaz <imshazbangash@gmail.com>
2021-07-24 15:49:41 +0500

    added list of articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Article } from './article/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-reddit';
-  addArticle(title:HTMLInputElement, link:HTMLInputElement):boolean{
-    console.log("addArticle", title.value, link.value);
-    return false
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'https://angular.io', 10),
+      new Article('Asp.net core', 'https://asp.com', 10),
+      new Article('Google', 'https://google.com'),
+    ];
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log('addArticle', title.value, link.value);
+    if (title.value != '' && link.value != '')
+      this.articles.push(new Article(title.value, link.value, 0));
+    return false;
   }
 }
